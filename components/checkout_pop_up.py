54d12617fb04283b2335e3cc313948a42d8abb8d
Andrei Neviadomski <andrei.neviadomskiy@gmail.com>
2026-02-23 17:47:18 +0100

    Added test test_remove_product_from_cart_popu

--- components/checkout_pop_up.py
+++ components/checkout_pop_up.py
@@ -5,3 +5,11 @@ class CheckoutPopUp(BasePage):
     
     def verify_adding_product_by_name(self):
         assert self.get_text(".line-clamp-2") == "Metal Brackets - Mini Size"
+
+    def remove_product_from_popup(self):
+        remove_button = self.page.locator("button.y-basket-card__remove").first
+        #expect(remove_button).to_be_visible()
+        remove_button.click()
+
+    def verify_product_removed_from_popup(self):
+        expect(self.page.locator(".line-clamp-2")).to_have_count(0)
---
+++ tests/test_cart_popup_flow.py
@@ -0,0 +1,64 @@
+import os
+from pages.home_page import HomePage
+from pages.category_page import CategoryPage
+from pages.product_page import ProductPage
+from components.header import Header
+from components.checkout_pop_up import CheckoutPopUp
+
+
+def test_add_product_to_cart(page):
+    home_page = HomePage(page)
+    category_page = CategoryPage(page)
+    product_page = ProductPage(page)
+    header = Header(page)
+    checkout_pop_up = CheckoutPopUp(page)
+
+    base_url = os.getenv("BASE_URL", "https://orthazone.com")
+
+    home_page.open(base_url)
+    home_page.check_title()
+
+    home_page.click_category_by_name("Bracket Systems")
+    category_page.click_category_by_name("Metal Brackets (Standard, Mini, Self-Ligating, Vertical Slot)")
+    category_page.click_category_by_name("Metal Brackets - Mini Size")
+    category_page.click_product_by_name("Metal Brackets - Mini")
+
+    product_page.select_option_value_by_name("MBT .018")
+    product_page.select_option_value_by_name("Hooks On 3")
+    product_page.select_option_value_by_name("Maxillary Right Canine (UR3)")
+    product_page.click_add_to_cart_button()
+
+    header.open_cart_popup()
+    checkout_pop_up.verify_adding_product_by_name()
+
+    home_page.take_screenshot("BRACKET_SYSTEM")
+
+
+def test_add_and_remove_product_in_cart_popup(page):
+    home_page = HomePage(page)
+    category_page = CategoryPage(page)
+    product_page = ProductPage(page)
+    header = Header(page)
+    checkout_pop_up = CheckoutPopUp(page)
+
+    base_url = os.getenv("BASE_URL", "https://orthazone.com")
+
+    home_page.open(base_url)
+    home_page.check_title()
+
+    home_page.click_category_by_name("Bracket Systems")
+    category_page.click_category_by_name("Metal Brackets (Standard, Mini, Self-Ligating, Vertical Slot)")
+    category_page.click_category_by_name("Metal Brackets - Mini Size")
+    category_page.click_product_by_name("Metal Brackets - Mini")
+
+    product_page.select_option_value_by_name("MBT .018")
+    product_page.select_option_value_by_name("Hooks On 3")
+    product_page.select_option_value_by_name("Maxillary Right Canine (UR3)")
+    product_page.click_add_to_cart_button()
+
+    header.open_cart_popup()
+    checkout_pop_up.verify_adding_product_by_name()
+    checkout_pop_up.remove_product_from_popup()
+    checkout_pop_up.verify_product_removed_from_popup()
+
+    home_page.take_screenshot("BRACKET_SYSTEM_REMOVE")
