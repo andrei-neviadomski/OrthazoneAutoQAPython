9f659e895cd0550eeea5fbe81adac531d7c550a6
Andrei Neviadomski <andrei.neviadomskiy@gmail.com>
2026-02-26 19:01:28 +0100

    rolled back some updates

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,21 +6,7 @@ class ProductPage(BasePage):
     def select_option_value_by_name(self,name: str):
         self.click(f"label:has-text('{name}')")
 
-    #def click_add_to_cart_button(self):
-        #self.click("a:has-text('Add to Cart')")
-        #cart_counter = self.page.locator(".int-cart-text-indicator").filter(visible=True).first
-        #expect(cart_counter).not_to_have_text("0", timeout=15000)
-
     def click_add_to_cart_button(self):
-        with self.page.expect_response(
-            lambda r: "checkout/cart/add" in r.url and r.status == 200,
-            timeout=30000
-        ) as cart_add_response:
-            with self.page.expect_response(
-                lambda r: "route=module/cart" in r.url and r.status == 200,
-                timeout=30000
-            ):
-                self.click("a:has-text('Add to Cart')")
-
-        body = cart_add_response.value.json()
-        assert "success" in body, f"Add to Cart failed. Server response: {body}"
+        self.click("a:has-text('Add to Cart')")
+        cart_counter = self.page.locator(".int-cart-text-indicator").filter(visible=True).first
+        expect(cart_counter).not_to_have_text("0", timeout=15000)
