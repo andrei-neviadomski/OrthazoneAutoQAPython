e4a8277d4e42ca3b581bae30fe2206ed1ebc231f
Andrei Neviadomski <andrei.neviadomskiy@gmail.com>
2026-02-26 15:33:05 +0100

    update category_page.py

--- pages/category_page.py
+++ pages/category_page.py
@@ -6,14 +6,14 @@ class CategoryPage(BasePage):
     #    self.click(f"span:has-text('{name}')")
 
     def click_category_by_name(self, name: str):
-        category_locator = self.page.locator(f"span:has-text('{name}')")
+        category_locator = self.page.locator(f"span.catagory-wrap__item--name:has-text('{name}')")
         category_locator.wait_for(state="visible", timeout=20000)
-        category_locator.click
+        category_locator.click()
 
     #def click_product_by_name(self, name: str):
     #    self.click(f"a.prodcard__link:has-text('{name}')")
 
     def click_product_by_name(self, name: str):
-        product_locator = self.page.locator(f"a.prodcard__link:has-text('{name}')")
+        product_locator = self.page.locator(f"a.prodcard__link:has-text('{name}')").first
         product_locator.wait_for(state="visible", timeout=20000)
-        product_locator.click
+        product_locator.click()
